62e82e29c145724e42df77ed1ddc3080337d2529
Eugene Monnier <genehmonnier@gmail.com>
2019-10-30 17:43:45 -0700

    created game logic inside functions

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,19 +3,21 @@
 const cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-var cardOne = cards[2];
-var cardTwo = cards[0];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again");
-	}
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again");
+		}
+	}	
 }
-//console.log("User flipped " + cardThree);
+
+function flipCard(cardId){
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId])
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
